7241b2cbe329a56fc7905cb65c3fd093db5a87a0
Rouji <rj@r0.at>
2018-06-01 02:14:25 +0200

    impl. epwing search

---
+++ epwing/epw.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# coding = utf-8
+
+from entrypoint2 import entrypoint
+from epwdb import EpwDB
+
+@entrypoint
+def main(heading_regex=None,
+         dict_regex=None,
+         base_dir=None,
+         prepare=False,
+         list_dicts=False,
+         limit=None,
+         format='[{dict}] {heading}:\n{text}\n'):
+    """Search EPWING dictionaries.
+
+    heading_regex: Regex to use for searching entry headings.
+    dict_regex: Regex to match against dictionary names.
+    list_dicts: List currently usable dictionaries.
+    prepare: Prepare an EPWING dictionary for use. Put your EPWING directories in in the epwing_dicts dir before calling this.
+    base_dir: Base dir for looking for the epwing_dicts folder and storing prepared dicts in.
+    format: Output format for search results. Uses Python's str.format(), passes variables dict,heading,text. Ex.: '[{dict}] {heading}:\\n{text}\\n'
+    limit: Limit number of results output."""
+    db = EpwDB(base_dir)
+    if prepare:
+        db.prepare()
+
+    if list_dicts:
+        print('\n'.join(db.dicts()))
+        return
+
+    limit = int(limit) if type(limit) is str else -1
+    for res in db.search(dict_regex, heading_regex):
+        print(format.format(**res))
+        limit -= 1
+        if limit == 0:
+            break
--- epwing/epwdb.py
+++ epwing/epwdb.py
@@ -1,6 +1,7 @@
+import re
 import gzip
 import json
-from typing import List, Dict
+from typing import List, Dict, Union
 from os.path import join, exists, dirname
 from os import makedirs, listdir, environ, errno
 from shutil import which
@@ -16,7 +17,8 @@ class EpwDB(object):
     DEFAULT_BASE = _DEFAULT_BASE
 
     def __init__(self, base_dir: str = _DEFAULT_BASE):
-        self.base_dir = base_dir
+        self.base_dir = base_dir or self.DEFAULT_BASE
+        self.dirs = {d: join(self.base_dir, d) for d in ['epwing_dicts', 'json']}
 
     def parse_epwing(self, path: str) -> List[Dict]:
         env = environ.copy()
@@ -33,25 +35,44 @@ class EpwDB(object):
             else:
                 raise
 
+        required_keys = {'heading', 'text'}
         for b in json.loads(out)['subbooks']:
             for e in b['entries']:
-                yield e
+                if set(e.keys()) >= required_keys:
+                    yield e
 
     def prepare(self):
-        dirs = {d: join(self.base_dir, d) for d in ['epwing_dicts', 'json']}
-        for d in dirs.values():
+        for d in self.dirs.values():
             if not exists(d):
                 makedirs(d)
 
         def proc(epwdict):
-            json_path = join(dirs['json'], epwdict)
+            json_path = join(self.dirs['json'], epwdict)
             if not exists(json_path):
                 with gzip.open(json_path, 'wt') as gz:
-                    gz.write(json.dumps(list(self.parse_epwing(join(dirs['epwing_dicts'], epwdict)))))
+                    json.dump(list(self.parse_epwing(join(self.dirs['epwing_dicts'], epwdict))), gz)
 
         with ThreadPoolExecutor(4) as executor:
-            futs = [executor.submit(proc,d) for d in listdir(dirs['epwing_dicts'])]
+            futs = [executor.submit(proc,d) for d in listdir(self.dirs['epwing_dicts'])]
             wait(futs)
 
+    def search(self, dict_re: Union[str, None] = None, heading_re: Union[str, None] = None) -> List[dict]:
+        dicts = self.dicts()
+        if dict_re:
+            dict_re = re.compile(dict_re)
+            dicts = [d for d in dicts if dict_re.search(d)]
+        if not dicts:
+            return []
+        if heading_re:
+            heading_re = re.compile(heading_re)
+        for d in dicts:
+            with gzip.open(join(self.dirs['json'], d)) as gz:
+                for entry in json.load(gz):
+                    if heading_re and not heading_re.search(entry['heading']):
+                        continue
+                    entry['dict'] = d
+                    yield entry
+
+
     def dicts(self):
-        return listdir(join(self.base_dir, 'json'))
+        return listdir(self.dirs['json'])
